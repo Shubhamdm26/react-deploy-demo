18ace6f94abeed2ea26df3656d5762b635f74ab8
Shubham <shubhamdm26@gmail.com>
2026-05-17 13:58:02 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div>
+      <h1>My first Deployment</h1>
+      <p>Learning CI/CD as React Developer</p>
+     </div>
+    </>
+  )
+}
+
+export default App
